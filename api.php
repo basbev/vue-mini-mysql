5f49e452f1e04308bcefeaac249759e785932055
Nutdanai <32787607+basbev@users.noreply.github.com>
2018-06-05 08:48:52 +0700

    Add files via upload

---
+++ api.php
@@ -0,0 +1,11 @@
+<?php
+
+$conn = new mysqli("localhost", "root","","detail");
+if($conn->connect_error){
+  die("Could not connect to database");
+}
+else {
+  die("Sucess");
+//  https://www.youtube.com/watch?v=juhcGTfsFeM
+}
+ ?>
