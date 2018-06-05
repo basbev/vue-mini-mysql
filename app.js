aa6333a8d01a5d8a0313a380f59f4cbbe7ada2de
Nutdanai <32787607+basbev@users.noreply.github.com>
2018-06-05 11:39:27 +0700

    Add axios
    axios

--- app.js
+++ app.js
@@ -4,17 +4,28 @@ var app = new Vue({
       showingAddModal: false,
       showingEditModal: false,
       showingDeleteModal: false,
-      errorMessage: "some errorMessage",
-      successMessage: "some successMessage",
-
+      errorMessage: "",
+      successMessage: "",
+      users: []
+      newUser: {username: "", email: "", mobile: ""}
   },
 
   mounted: function(){
     console.log("mounted");
-    console.error();
+    this.getAllUsers();
   },
 
   methods: {
-
+    getAllUsers: function(){
+      axios.get("http://localhost/vue-mini-mysql/api.php?action=read")
+      .then(function(response){
+        //console.log(response);
+        if(response.data.error){
+          app.errorMessage = response.data.message;
+        } else{
+          app.users = response.data.users;
+        }
+      });
+    }
   }
 })
